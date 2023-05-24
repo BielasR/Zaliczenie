e5e4bbf5b8c1df4e373063e3f557cda2ec0469bf
BielasR <126687810+BielasR@users.noreply.github.com>
2023-05-24 12:33:41 +0000

    z

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+import java.util.Random;
 class Main 
 {
   public static void main(String[] args) 
   {
-    System.out.println("Hello world!");
+    Random K =new Random();
+
+    int K1 = K.nextInt(6) + 1;
+    int K2 = K.nextInt(6) + 1;
+
+    System.out.println("Wynik kostki 1: " + K1);
+    System.out.println("Wynik kostki 2: " + K2);
+
+    if(K1 == K2)
+    {
+      System.out.println("Wygrana");
+    }
+     else
+       System.out.println("Przegrana"); 
+
+    
   }
 }
